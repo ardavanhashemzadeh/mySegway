7aaa39cb59237c38ded17364be291edc8fc1a9f8
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2018-11-04 09:09:22 -0500

    Update motors.c

--- motors.c
+++ motors.c
@@ -6,10 +6,10 @@
 #define RANGE	100
 #define INITIAL_VALUE 0
 
-int MLS = 2;
-int MLD = 3;
-int MRS = 4;
-int MRD = 5;
+#define MLS 2 // Motor Left Speed
+#define MLD 3 // Motor Left Direction
+#define MRS 4 // Motor Right Speed
+#define MRD 5 // Motor Right Direction
 
 void init_motors()
 {
@@ -18,40 +18,23 @@ void init_motors()
   softPwmWrite(MLS, 0);
   pinMode(MLD, OUTPUT);
   digitalWrite(MLD, HIGH);
-
-  // initialize right motor (pin4, 5 & 6)
   softPwmCreate(MRS, INITIAL_VALUE, RANGE);
   softPwmWrite(MRS, 0);
   pinMode(MRD, OUTPUT);
   digitalWrite(MRD, HIGH);
 }
-
 void stop_motors()
 {
   softPwmWrite(MLS, 0);
   softPwmWrite(MRS, 0);
   // exit(1);
-
 }
-
 double left_speed;
 double right_speed;
-
 void motors(double speed, double left_offset, double right_offset)
 {
-
-// to come to me, drive pin 0&5 to some power
-// to away from me, drive pin 2&4 to some power
-
-// pin 0:LM+, 2:LM-, 3:LCE
-// pin 5:RM+, 4:RM-, 6:RCE
-// put M+ high & M- low will come to me
-// put M+ low & M- High will away from me
-
   left_speed = speed + left_offset;
   right_speed = speed + right_offset;
-
-  // left motor
   if (left_speed < 0)  {
     softPwmWrite(MLS, (int) -left_speed);
     digitalWrite(MLD, 0);
@@ -60,9 +43,7 @@ void motors(double speed, double left_offset, double right_offset)
   if (left_speed > 0)  {
     softPwmWrite(MLS, (int) left_speed);
     digitalWrite(MLD, 1);
-  }
-
-  // right motor
+  } 
   if (right_speed < 0)  {
     softPwmWrite(MRS, (int) -right_speed);
     digitalWrite(MRD, 0);
@@ -73,6 +54,3 @@ void motors(double speed, double left_offset, double right_offset)
     digitalWrite(MRD, 1);
   }
 }
-
-
-
